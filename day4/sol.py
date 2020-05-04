7dd53bbd4877c57cfbc740bbab5de908c051c487
Yogesh Kumar Rana <yankeekiloromeo@gmail.com>
2020-05-04 20:07:30 +0530

    day-4

--- day4/sol.py
+++ day4/sol.py
@@ -1,4 +1,7 @@
 class Solution:
     def findComplement(self, num: int) -> int:
+        # Take 1
+        return num^-1+2**len(bin(num)[2:])
+        # Take 2
         nb=(int)(math.floor(math.log(num)/math.log(2))) + 1
         return ((1<<nb)-1)^num
